8fd06a9023b53f737a7273a1092f691107c1b887
IloveNooodles <mgarebaldhie80@gmail.com>
2023-04-23 17:49:04 +0700

    feat: cleaned the data

---
+++ README.md
@@ -0,0 +1,19 @@
+# Six Scrapper
+
+This repository aims to get all of available matakuliah and dosen information from all of prodi and facutly
+
+## library used
+
+1. argparse
+2. bs4
+3. requests
+
+## How to run
+
+1. Clone this repository
+2. create virtual environment from python `virtualenv venv`
+3. install all requirements `pip install -r requirements.txt`
+4. provide your `nim` and `cookie` from ITB sso service
+   1. To get your cookie, you need to logged in in SIX
+   2. Right click > `inspect` > `application`
+   3. Grab the `khongguan` cookie from there
---
+++ clean.py
@@ -0,0 +1,39 @@
+import json
+
+DATA = None
+
+
+def main():
+    """
+    removed unavailable matakuliah
+    """
+    f = open("saved.json", "r")
+    data = json.load(f)
+    f.close()
+
+    new_data = {}
+    for fakultas in data:
+        new_data[fakultas] = {}
+        for prodi in data[fakultas]:
+            list_matkul_dosen = data[fakultas][prodi]
+            to_add = []
+            for matkul_dosen in list_matkul_dosen:
+                list_dosen = matkul_dosen["list_dosen"]
+                found_empty = False
+                for dosen in list_dosen:
+                    if "\n" in dosen:
+                        found_empty = True
+                        break
+
+                if not found_empty:
+                    to_add.append(matkul_dosen)
+
+            new_data[fakultas][prodi] = to_add
+
+    clean_data = json.dumps(new_data)
+    cleaned_data = open("cleaned.json", "w")
+    cleaned_data.write(clean_data)
+    cleaned_data.close()
+
+if __name__ == "__main__":
+    main()
